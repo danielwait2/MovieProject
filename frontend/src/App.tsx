159594849ea6bf478f3a5e614799723f60ffa99e
jnewell-7 <jacksonnewell07@gmail.com>
2025-04-08 12:27:58 -0600

    cleanup privacy policy and add footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import MoviePage from './pages/MoviePage';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
-import CreateAccountPage from './pages/CreateAccountPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
@@ -11,13 +10,12 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<MoviePage/>} />
-                <Route path='/movies' element={<MoviePage/>} />
-                <Route path='/login' element={<LoginPage/>} />
-                <Route path='/register' element={<RegisterPage/>} />
-                <Route path='/createAccount' element={<CreateAccountPage/>} />
-                <Route path='/privacyPolicy' element={<PrivacyPolicyPage/>} />
-                <Route path='/productDetails' element={<ProductDetailPage/>} />
+                <Route path="/" element={<MoviePage />} />
+                <Route path="/movies" element={<MoviePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/privacyPolicy" element={<PrivacyPolicyPage />} />
+                <Route path="/productDetails" element={<ProductDetailPage />} />
             </Routes>
         </Router>
     );
--- frontend/src/pages/MoviePage.tsx
+++ frontend/src/pages/MoviePage.tsx
@@ -2,36 +2,41 @@ import GenreFilter from '../components/GenreFilter';
 import MovieCarousel from '../components/MovieCarousel';
 import { useState } from 'react';
 
-
 // add this for login logout and authorize view
 import AuthorizeView, { AuthorizedUser } from '../components/AuthorizeView';
 import Logout from '../components/Logout';
+import Footer from '../components/Footer';
 const MoviePage = () => {
     const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
 
     return (
         <>
-        <AuthorizeView>
-            <span>
-                <Logout>
-                    Logout <AuthorizedUser value="email"/>
-                </Logout>
-            </span>
-            <div className="min-h-screen bg-[#121212] text-white">
-                <header className="p-6 border-b border-gray-800">
-                    <h1 className="text-3xl font-bold">Movies</h1>
-                </header>
-                <main>
-                <div className="row mb-5">
-                    <MovieCarousel />
+            <AuthorizeView>
+                <span>
+                    <Logout>
+                        Logout <AuthorizedUser value="email" />
+                    </Logout>
+                </span>
+                <div className="min-h-screen bg-[#121212] text-white">
+                    <header className="p-6 border-b border-gray-800">
+                        <h1 className="text-3xl font-bold">Movies</h1>
+                    </header>
+                    <main>
+                        <div className="row mb-5">
+                            <MovieCarousel />
+                        </div>
+                        <div className="row">
+                            <GenreFilter
+                                selectedGenres={selectedGenres}
+                                setSelectedGenres={setSelectedGenres}
+                            />
+                        </div>
+                    </main>
                 </div>
-                <div className="row">
-                    <GenreFilter selectedGenres={selectedGenres}
-                            setSelectedGenres={setSelectedGenres}/>
-                </div>
-            </main>
-            </div>
-        </AuthorizeView>
+                <br />
+                <br />
+                <Footer />
+            </AuthorizeView>
         </>
     );
 };
