4f22cf067f0ee27944e2ec3f63981bee5b0b37c8
Daniel Wait <danielwait6492@gmail.com>
2025-04-07 20:35:01 -0600

    putting back changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,20 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import MoviePage from './pages/MoviePage';
 import LoginPage from './pages/LoginPage';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
-import CreateAccountPage from './pages/CreateAccountPage';
+import  CreateAccountPage  from './pages/CreateAccountPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 
 function App() {
     return (
-        <div className="min-h-screen bg-[#121212] text-white">
-            <main>
-                <MovieCarousel />
-            </main>
-            <PrivacyPolicy />
-        </div>
+        <Router>
+            <Routes>
+                <Route path='/' element={<MoviePage/>} />
+                <Route path='/login' element={<LoginPage/>} />
+                <Route path='/createAccount' element={<CreateAccountPage/>} />
+                <Route path='/privacyPolicy' element={<PrivacyPolicyPage/>} />
+                <Route path='/productDetails' element={<ProductDetailPage/>} />
+            </Routes>
+        </Router>
     );
 }
 
