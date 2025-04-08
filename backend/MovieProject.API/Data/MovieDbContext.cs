03255a47c78e8774ab28fc378a6b7a6ca0b6169f
Daniel Wait <danielwait6492@gmail.com>
2025-04-08 12:23:26 -0600

    added in recommendations api might work

--- backend/MovieProject.API/Data/MovieDbContext.cs
+++ backend/MovieProject.API/Data/MovieDbContext.cs
@@ -9,6 +9,7 @@ namespace MovieProject.API.Data
         }
 
         // Each DbSet corresponds to a table
+        public DbSet<Recommendation> MovieRecomendation { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Rating> Ratings { get; set; }
         public DbSet<User> Users { get; set; }
