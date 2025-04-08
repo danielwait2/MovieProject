730a7c8c60fd2b5968be58bacb7ab018ecfb187a
Daniel Wait <danielwait6492@gmail.com>
2025-04-08 12:26:35 -0600

    fixed app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import MoviePage from './pages/MoviePage';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
-import CreateAccountPage from './pages/CreateAccountPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
@@ -16,7 +15,6 @@ function App() {
                 <Route path="/movies" element={<MoviePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="/createAccount" element={<CreateAccountPage />} />
                 <Route path="/privacyPolicy" element={<PrivacyPolicyPage />} />
                 <Route path="/productDetails" element={<ProductDetailPage />} />
                 <Route path="/admin" element={<AdminMoviePage />} />
