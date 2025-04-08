a7a3db525a78a76f5312aaf7c9961ab97638c61e
Daniel Wait <danielwait6492@gmail.com>
2025-04-08 08:58:19 -0600

    cookie concent on login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/Identity.css';
+import CookieConsent from "react-cookie-consent";
+
 
 function LoginPage() {
     // state variables for email and passwords
@@ -159,6 +161,7 @@ function LoginPage() {
                     </div>
                 </div>
             </div>
+            <CookieConsent>This website uses cookies to enhance the user experience.</CookieConsent>
         </div>
     );
 }
